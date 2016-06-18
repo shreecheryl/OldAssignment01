5bd4050ad81d67483493fe008da69135a7210b52
Cheryl Davis <shreecheryl@abs-macbook-air.attlocal.net>
2016-06-17 19:07:30 -0700

    adding files for Assignment01

---
+++ scripts.js
@@ -0,0 +1,131 @@
+// STEP 1
+/*
+var someMonth;
+function theMonth();
+currentMonth;
+var summerMonth;
+var myLibraryAFunction;
+*/
+
+// STEP 2
+/*
+2 + 3           //numeric literal expression
+"Cheryl Davis"  //string literal expression
+false           //Boolean literal expression
+null            //null literal expression
+*/
+
+// STEP 3
+/*
+var total = 2 + 3;
+var fullName = "Cheryl Davis";
+*/
+
+// STEP 4
+/*
+var firstName;
+var lastName;
+var address;
+var city;
+var state;
+var zipCode;
+var age;
+var referralSource;
+var okToContact;
+*/
+
+// STEP 5
+/*
+var firstName = "Cheryl";
+var lastName = prompt("Enter your last name");
+var address;
+address = "666 S Madison Ave";
+var city = "Pasadena";
+var state = states[4];
+var zipCode = "92109";
+var age = 53;
+var referralSource = "internet";
+var okToContact = false;
+*/
+
+// STEP 6
+/*
+var combo1 = 49 + "birds";
+console.log(combo1);
+var combo2 = true + "sign";
+console.log(combo2);
+var combo3 = 108 + true;
+console.log(combo3);
+*/
+
+// STEP 7
+/*
+console.log(firstName);
+var firstName = "Cheryl";
+*/
+
+// STEP 8
+// I changed "I'm" to "I\'m" so the string would be valid
+/*
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'; console.log(someString);
+*/
+
+// STEP 9
+/*
+var person1 = null;
+console.log(person1);
+var person2;
+console.log(person2);
+*/
+
+// STEP 10
+/*
+console.log(typeof "Cheryl Davis");
+console.log(typeof 53);
+console.log(typeof false);
+console.log(typeof [1, 2, 3, 4]);
+console.log(typeof firstName);
+*/
+
+// STEP 11
+/*
+alert("Hello " + "Cheryl Davis" + ", welcome to the Javascript class!");
+*/
+
+// STEP 12
+/*
+var name = "Cheryl Davis";
+alert("Hello " + name + ", welcome to the Javascript class!");
+*/
+
+// STEP 13
+/*
+var course = "JavaScript";
+alert("Hello " + name + ", welcome to the " + course + " class!");
+*/
+
+// STEP 14
+/*
+alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+*/
+
+// STEP 15
+/*
+var name = prompt("Enter your first and last name");
+alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+*/
+
+// STEP 16
+/*
+var course = prompt("What class are you taking?");
+alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+*/
+
+
+
+
+
+
+
+
+
